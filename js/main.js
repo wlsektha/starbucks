3c616b7124ec975fe8329253fabf2a305318f223
wlsektha <smilejds00@naver.com>
2022-02-23 20:55:52 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// 요소 검색 및 찾기
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  // setAttribute 는 속성 지정
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
 
 // badge
 // 쿼리셀렉터를 통해 특정 요소 찾기
@@ -151,10 +135,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2022 생성 textContent- 글자내용으로
 
 
 
